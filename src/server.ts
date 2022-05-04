e35f6508bf0e906094b933e6039079d252b401e4
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-05-04 20:47:43 -0300

    :star: feat(backend): create feedbacks endpoint
    - Add feedback data to database

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-const teste = 1;
+import express from 'express';
+import { prisma } from './prisma';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/feedbacks', async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+  
+  const feedback = await prisma.feedback.create({
+    data: {
+      type: type,
+      comment: comment,
+      screenshot: screenshot
+    }
+  })
+
+  return response.status(201).json({ data: feedback });
+});
+
+app.listen(3333, () => {
+  console.log('🚀 HTTP server running');
+});
